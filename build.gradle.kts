56c2b9ff4197a65b0490efb96edb43910c4366f0
Daniel Orr <orrdaniel73@gmail.com>
2024-03-21 08:44:18 +0000

    DatabaseConfiguration

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.fabricmc.net/")
+    maven("https://mvn.lumine.io/repository/maven-public/")
+    maven("https://maven.andante.dev/releases/")
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("com.mojang:datafixerupper:6.0.6")
+    implementation("dev.andante:codex:1.4.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
