f13372cf04159a450718e0214b097306c49d001b
Daniel Orr <orrdaniel73@gmail.com>
2024-10-01 16:32:53 +0100

    Update and correctly define schemas

--- src/main/kotlin/net/mcbrawls/api/database/schema/BrawlsDatabaseSchema.kt
+++ src/main/kotlin/net/mcbrawls/api/database/schema/BrawlsDatabaseSchema.kt
@@ -9,6 +9,7 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insertIgnore
 import org.jetbrains.exposed.sql.json.json
+import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestamp
 import org.jetbrains.exposed.sql.kotlin.datetime.date
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
@@ -57,13 +58,15 @@ enum class DbChatMode(val id: String) {
 }
 
 object ApiKeys : Table("ApiKeys") {
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val apiKey = varchar("api_key", 32)
+
+    override val primaryKey = PrimaryKey(playerId, apiKey)
 }
 
 object ChatLogs : Table("ChatLogs") {
     val logId = integer("log_id").autoIncrement()
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val worldId = varchar("world_id", 255)
     val contemporaryUsername = varchar("contemporary_username", 16)
     val message = text("message")
@@ -72,44 +75,53 @@ object ChatLogs : Table("ChatLogs") {
     val chatResult = customEnumeration("chat_result", toDb = ChatResult::id, fromDb = ChatResult::fromId)
     val gameInstanceUuid = varchar("game_instance_uuid", UUID_VARCHAR_LENGTH).nullable()
     val partyLeaderUuid = varchar("party_leader_uuid", UUID_VARCHAR_LENGTH).nullable()
-    val timestamp = timestamp("timestamp")
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
+
+    override val primaryKey = PrimaryKey(logId)
 }
 
 object Friends : Table("Friends") {
-    val relationId = integer("relation_id").autoIncrement()
-    val initiator = varchar("initiator", UUID_VARCHAR_LENGTH)
-    val recipient = varchar("recipient", UUID_VARCHAR_LENGTH)
-    val since = timestamp("since")
-    val mutual = bool("mutual")
+    val initiator = reference("initiator", Players.playerId)
+    val recipient = reference("recipient", Players.playerId)
+    val since = timestamp("since").defaultExpression(CurrentTimestamp)
+    val mutual = bool("mutual").default(false)
+
+    override val primaryKey = PrimaryKey(initiator, recipient)
 }
 
 object GameInstances : Table("GameInstances") {
     val instanceId = integer("instance_id").autoIncrement()
     val uuid = varchar("uuid", UUID_VARCHAR_LENGTH)
-    val gameType = varchar("game_type", 100)
+    val gameType = varchar("game_type", 100).default("unknown")
     val participants = json<JsonArray>("participants", jsonConfig)
     val additionalData = json<JsonArray>("additional_data", jsonConfig).nullable()
+
+    override val primaryKey = PrimaryKey(instanceId)
 }
 
 object IgnoredPlayers : Table("IgnoredPlayers") {
-    val receiver = varchar("receiver", UUID_VARCHAR_LENGTH)
-    val target = varchar("target", UUID_VARCHAR_LENGTH)
+    val receiver = reference("receiver", Players.playerId)
+    val target = reference("target", Players.playerId)
+
+    override val primaryKey = PrimaryKey(receiver, target)
 }
 
 object IpAddresses : Table("IpAddresses") {
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val address = varchar("address", 16)
+
+    override val primaryKey = PrimaryKey(playerId, address)
 }
 
 object Medals : Table("Medals") {
-    val id = integer("id").autoIncrement()
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val medalId = varchar("medal_id", 100)
-    val timestamp = timestamp("timestamp")
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
+
+    override val primaryKey = PrimaryKey(playerId, medalId)
 }
 
 object Partnerships : Table("Partnerships") {
-    val partnerId = integer("partner_id").autoIncrement()
     val partnerName = text("partner_name")
     val status = customEnumeration("status", toDb = PartnerStatus::id, fromDb = PartnerStatus::fromId)
     val createdDate = date("created_date")
@@ -121,66 +133,79 @@ object Partnerships : Table("Partnerships") {
 }
 
 object PlayerCosmetics : Table("PlayerCosmetics") {
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH).autoIncrement()
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val title = varchar("title", 100).nullable()
+
+    override val primaryKey = PrimaryKey(playerId)
 }
 
 object PlayerReports : Table("PlayerReports") {
     val reportId = integer("report_id").autoIncrement()
-    val reporterId = varchar("reporter_id", UUID_VARCHAR_LENGTH)
-    val offenderId = varchar("offender_id", UUID_VARCHAR_LENGTH)
+    val reporterId = reference("reporter_id", Players.playerId).index()
+    val offenderId = reference("offender_id", Players.playerId).index()
     val reason = text("reason")
-    val timestamp = timestamp("timestamp")
-    val resolved = bool("resolved")
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
+    val resolved = bool("resolved").default(false)
 }
 
 object Players : Table("Players") {
     val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
     val firstJoin = timestamp("first_join")
     val lastJoin = timestamp("last_join").nullable()
+
+    override val primaryKey = PrimaryKey(playerId)
 }
 
 object PlayerSettings : Table("PlayerSettings") {
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val settingKey = varchar("setting_key", 50)
     val settingValue = varchar("setting_value", 50)
+
+    override val primaryKey = PrimaryKey(playerId, settingKey)
 }
 
 object Punishments : Table("Punishments") {
     val punishmentId = integer("punishment_id").autoIncrement()
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId)
     val contemporaryName = varchar("contemporary_name", 16)
     val officerId = varchar("officer_id", UUID_VARCHAR_LENGTH).nullable()
     val punishmentType = customEnumeration("punishment_type", toDb = PunishmentType::id, fromDb = PunishmentType::fromId)
     val reason = text("reason").nullable()
     val playerMadeAware = bool("player_made_aware")
-    val acknowledged = bool("acknowledged")
-    val timestamp = timestamp("timestamp")
+    val acknowledged = bool("acknowledged").default(false)
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
     val duration = long("duration").nullable()
+
+    override val primaryKey = PrimaryKey(punishmentId)
 }
 
 object Purchases : Table("Purchases") {
-    val purchaseId = integer("purchase_id").autoIncrement()
     val transactionId = varchar("transaction_id", 40)
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId).index()
     val packageId = integer("package_id")
     val packageExpiry = integer("package_expiry")
     val packageName = text("package_name")
-    val timestamp = timestamp("timestamp")
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
+
+    override val primaryKey = PrimaryKey(transactionId, packageId)
 }
 
 object StatisticEvents : Table("StatisticEvents") {
     val eventId = integer("event_id").autoIncrement()
-    val playerId = varchar(PLAYER_ID_KEY, UUID_VARCHAR_LENGTH)
+    val playerId = reference(PLAYER_ID_KEY, Players.playerId).index()
     val causeId = text("cause_id")
     val gameType = varchar("game_type", 100).nullable()
     val gameUuid = varchar("game_uuid", UUID_VARCHAR_LENGTH).nullable()
     val experienceAmount = integer("experience_amount")
-    val timestamp = timestamp("timestamp")
+    val timestamp = timestamp("timestamp").defaultExpression(CurrentTimestamp)
+
+    override val primaryKey = PrimaryKey(eventId)
 }
 
 object LuckPermsPlayers : Table("luckperms_players") {
     val uuid = varchar("uuid", UUID_VARCHAR_LENGTH)
-    val username = varchar("username", 16)
+    val username = varchar("username", 16).index()
     val primaryGroup = varchar("primary_group", 36)
+
+    override val primaryKey = PrimaryKey(uuid)
 }
