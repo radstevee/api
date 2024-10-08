3b9e8f9b963087604a60b2b3fd8c1a6f8a50eace
Daniel Orr <orrdaniel73@gmail.com>
2024-10-08 12:03:56 +0100

    Clean up CachedDatabaseValue

--- src/main/kotlin/net/mcbrawls/api/database/CachedDatabaseValue.kt
+++ src/main/kotlin/net/mcbrawls/api/database/CachedDatabaseValue.kt
@@ -1,13 +1,11 @@
 package net.mcbrawls.api.database
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
+import net.mcbrawls.api.runAsync
 
 /**
  * A database value that does not need to be fetched every time it is required.
  * This object tracks changes to the database value as changes to the database are made.
  */
-@Suppress("DeferredResultUnused")
 class CachedDatabaseValue<T>(
     /**
      * The default value.
@@ -17,48 +15,47 @@ class CachedDatabaseValue<T>(
     /**
      * The function to select the true value from the database.
      */
-    private val databaseSelectionFunction: DatabaseSelectionFunction<T>
+    private val selector: Selector<T>
 ) {
     init {
-        GlobalScope.async {
+        runAsync {
             // refresh value from database
             refreshValue()
         }
     }
 
-    @field:Volatile
-    private var currentValue: T = defaultValue
-
     /**
      * The value of this instance.
      */
-    val value: T get() = currentValue
+    @field:Volatile
+    var value: T = defaultValue
+        private set
 
     /**
      * Modifies the value locally and on the database according to the function provided.
      * @return the new local value
      */
-    suspend fun modifyValue(function: CalculatedDatabaseModifyFunction<T>): T {
-        return function.modify(currentValue).also { currentValue = it }
+    suspend fun modifyValue(modifier: ValueModifier<T>): T {
+        return modifier.modify(value).also { modifiedValue -> value = modifiedValue }
     }
 
     /**
      * Refreshes the current value from the database.
      */
     suspend fun refreshValue() {
-        val value = databaseSelectionFunction.select(defaultValue)
-        currentValue = value
+        val selectedValue = selector.select(defaultValue)
+        value = selectedValue
     }
 
-    fun interface CalculatedDatabaseModifyFunction<T> {
+    fun interface ValueModifier<T> {
         /**
-         * Calculates the change to the database and makes the database change.
+         * Calculates the change to the database (for local storage) and makes the database change.
          * @return the new cached value
          */
         suspend fun modify(currentValue: T): T
     }
 
-    fun interface DatabaseSelectionFunction<T> {
+    fun interface Selector<T> {
         /**
          * Selects the value from the database.
          * @return the deferred result
